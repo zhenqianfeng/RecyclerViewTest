9aa8c139e0eb170c0d0390480ad48e898e8f0801
zhenqianfeng <ferretfeng@yahoo.com>
2019-03-09 14:53:09 -0500

    class activity test

---
+++ app/src/main/java/com/example/recyclerviewtest/BookAdapter.java
@@ -0,0 +1,65 @@
+package com.example.recyclerviewtest;
+
+import android.service.autofill.TextValueSanitizer;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.MyViewHolder>{
+
+    List<Book> bookList;
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView name, pages, isbn;
+        Button b;
+
+        MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.txt_name);
+            pages = view.findViewById(R.id.txt_page);
+            isbn = view.findViewById(R.id.txt_ISBN);
+            b = view.findViewById(R.id.btn_test);
+        }
+    }
+
+    BookAdapter(List<Book> bookList) {this.bookList = bookList;}
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.book_row, viewGroup,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final BookAdapter.MyViewHolder viewHolder, int i) {
+        final Book book=bookList.get(i);
+        viewHolder.name.setText(book.getName());
+        viewHolder.pages.setText(book.getPage()+"");
+        viewHolder.isbn.setText(book.getISBN()+"");
+
+        viewHolder.b.setOnClickListener(new View.OnClickListener() {
+            String showMsg;
+            @Override
+            public void onClick(View v) {
+                if(book.isLent())showMsg = "Not available";
+                else showMsg = "Available";
+                Toast.makeText(v.getContext(),showMsg,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.bookList.size();
+    }
+
+}
+
--- app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.recyclerviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Book> bookList = new ArrayList<>();
+    private BookAdapter ba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        ba = new BookAdapter(bookList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(ba);
+        bookList.addAll(generatedData());
+    }
+
+    protected ArrayList<Book> generatedData(){
+        ArrayList<Book> alb = new ArrayList<>();
+        Book b;
+
+        b=new Book(1233333,"Dune",450,false);
+        alb.add(b);
+        b=new Book(4569087,"Star wars",789,true);
+        alb.add(b);
+        b=new Book(7893452,"Star trek",852,false);
+        alb.add(b);
+        b=new Book(1233333,"Dune",450,false);
+        alb.add(b);
+        b=new Book(4569087,"Star wars",789,true);
+        alb.add(b);
+        b=new Book(7893452,"Star trek",852,false);
+        alb.add(b);
+        b=new Book(1233333,"Dune",450,false);
+        alb.add(b);
+        b=new Book(4569087,"Star wars",789,true);
+        alb.add(b);
+        b=new Book(7893452,"Star trek",852,false);
+        alb.add(b);
+        return alb;
     }
 }
